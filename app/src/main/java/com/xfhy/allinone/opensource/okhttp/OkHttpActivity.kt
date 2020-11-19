ce5bb7f2232b8a1c5cdfa978631a0f9bdc2b83c7
xfhy <xfhy666@qq.com>
2020-11-20 07:47:20 +0800

    OkHttp 核心逻辑

--- app/src/main/java/com/xfhy/allinone/opensource/okhttp/OkHttpActivity.kt
+++ app/src/main/java/com/xfhy/allinone/opensource/okhttp/OkHttpActivity.kt
@@ -10,6 +10,10 @@ import kotlinx.coroutines.*
 import okhttp3.*
 import java.io.IOException
 
+/**
+ * OkHttp核心: 通过拦截器组成的责任链,依次经过重试、桥接、缓存、连接和访问服务器等过程,来获取到一个响应并交给用户
+ * 其中,缓存和连接两部分是重点.前者涉及到一些计算机网络的基础知识,后者则是OkHttp效率和框架的核心(连接池复用真的强).
+ */
 class OkHttpActivity : AppCompatActivity() {
 
     object OkHttpManager {
@@ -50,7 +54,8 @@ class OkHttpActivity : AppCompatActivity() {
     private fun testSync() {
         mainScope.launch {
             val async = async(Dispatchers.IO) {
-                val request = Request.Builder().url("https://www.baidu.com/").build()
+                val builder = Request.Builder()
+                val request = builder.url("https://www.baidu.com/").build()
                 val response = OkHttpManager.okHttpClient.newCall(request).execute()
                 val responseBody = response.body()
                 responseBody?.string().logString()
