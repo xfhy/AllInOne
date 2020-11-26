a304a92cfedde63b689338aed040bb096f5e6a2b
xfhy <xfhy666@qq.com>
2020-11-27 07:42:30 +0800

    1. add Application 2. compatible http

---
+++ app/src/main/java/com/xfhy/allinone/App.kt
@@ -0,0 +1,16 @@
+package com.xfhy.allinone
+
+import com.xfhy.library.common.BaseApplication
+
+/**
+ * @author : xfhy
+ * Create time : 2020/11/27 7:34 AM
+ * Description :
+ */
+public class App : BaseApplication() {
+
+    companion object {
+        fun getAppContext() = context
+    }
+
+}
