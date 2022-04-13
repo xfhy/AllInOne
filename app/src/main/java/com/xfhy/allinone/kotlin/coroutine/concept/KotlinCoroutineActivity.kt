450673fbb78d8a99d2f3bc3d1674a0c984efd07f
xfhy <xfhy666@qq.com>
2022-04-13 08:24:07 +0800

    kotlin协程 取消

--- app/src/main/java/com/xfhy/allinone/kotlin/coroutine/concept/KotlinCoroutineActivity.kt
+++ app/src/main/java/com/xfhy/allinone/kotlin/coroutine/concept/KotlinCoroutineActivity.kt
@@ -10,6 +10,7 @@ import kotlinx.android.synthetic.main.activity_kotlin_coroutine.*
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 /**
  * @author : xfhy
@@ -65,6 +66,51 @@ class KotlinCoroutineActivity : TitleBarActivity() {
             job.cancel()
             log("Done")
         }
+    }
+
+    //取消不了
+    fun testCancellationIsNotPossible(view: View) {
+        val startTime = System.currentTimeMillis()
+        lifecycleScope.launch {
+            withContext(Dispatchers.IO) {
+                var nextPrintTime = startTime
+                var i = 0
+                while (i < 15) {
+                    if (System.currentTimeMillis() >= nextPrintTime) {
+                        log("Hello ${i++}")
+                        nextPrintTime += 500L
+                    }
+                    if (i == 5) {
+                        finish()
+                    }
+                }
+            }
+            log("Done")
+        }
+        //因为在5的时候,进行了finish,所以onDestroy被打印出来了. 这个时候其实lifecycleScope已经cancel了,然而withContext并没有结束,因为它没有感知到已经cancel了,继续执行,一直到执行完成.
+        //但最后的Done没有打印出来,因为已经cancel了,不会再切线程回来执行了.
+        //打印结果:
+        //2022-04-13 08:11:25.494 15739-15794/com.xfhy.allinone D/xfhy_tag: Hello 0
+        //2022-04-13 08:11:25.931 15739-15794/com.xfhy.allinone D/xfhy_tag: Hello 1
+        //2022-04-13 08:11:26.431 15739-15794/com.xfhy.allinone D/xfhy_tag: Hello 2
+        //2022-04-13 08:11:26.931 15739-15794/com.xfhy.allinone D/xfhy_tag: Hello 3
+        //2022-04-13 08:11:27.431 15739-15794/com.xfhy.allinone D/xfhy_tag: Hello 4
+        //2022-04-13 08:11:27.931 15739-15794/com.xfhy.allinone D/xfhy_tag: Hello 5
+        //2022-04-13 08:11:27.999 15739-15739/com.xfhy.allinone D/xfhy_tag: onDestroy
+        //2022-04-13 08:11:28.431 15739-15794/com.xfhy.allinone D/xfhy_tag: Hello 6
+        //2022-04-13 08:11:28.931 15739-15794/com.xfhy.allinone D/xfhy_tag: Hello 7
+        //2022-04-13 08:11:29.431 15739-15794/com.xfhy.allinone D/xfhy_tag: Hello 8
+        //2022-04-13 08:11:29.931 15739-15794/com.xfhy.allinone D/xfhy_tag: Hello 9
+        //2022-04-13 08:11:30.431 15739-15794/com.xfhy.allinone D/xfhy_tag: Hello 10
+        //2022-04-13 08:11:30.931 15739-15794/com.xfhy.allinone D/xfhy_tag: Hello 11
+        //2022-04-13 08:11:31.431 15739-15794/com.xfhy.allinone D/xfhy_tag: Hello 12
+        //2022-04-13 08:11:31.931 15739-15794/com.xfhy.allinone D/xfhy_tag: Hello 13
+        //2022-04-13 08:11:32.431 15739-15794/com.xfhy.allinone D/xfhy_tag: Hello 14
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        log("onDestroy")
     }
 
 }
